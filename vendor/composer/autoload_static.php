cb75aa74e967c3add7aa9dc790df1959f52aeb17
Rafael Luz <rafa.tecnologo@hotmail.com>
2022-08-12 17:16:24 -0300

    - phpUnit

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -40,8 +40,8 @@ class ComposerStaticInit3ce92eccd693955bc100304768dfcf21
         'phpDocumentor\\Reflection\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpdocumentor/reflection-common/src',
-            1 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
-            2 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            1 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            2 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
         ),
         'Webmozart\\Assert\\' => 
         array (
